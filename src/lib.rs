2d84e34cf6bea32cbb4372d250ee5c3e2fb98d05
cde123456781 <bjc199@uclive.ac.nz>
2026-09-21 00:40:01 +1200

    Implements Emu struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,44 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub const SCREEN_HEIGHT: usize = 64;
+pub const SCREEN_WIDTH: usize = 32;
+
+const RAM_SIZE: usize = 4096;
+const NUM_REGISTERS: usize = 16;    
+const STACK_SIZE: usize = 16;
+const NUM_KEYS: usize = 16;
+const START_ADDRESS: u16 = 0x200;
+
+
+pub struct Emu {
+    pc: u16,
+    ram: [u8; RAM_SIZE],
+    screen: [bool; SCREEN_HEIGHT * SCREEN_WIDTH],
+    v_reg: [u8; NUM_REGISTERS],
+    i_reg: u16,
+    sp: u16,
+    stack: [u16; STACK_SIZE],
+    keys: [bool; NUM_KEYS],
+    dt: u8,
+    st: u8,
+
+
+
+
+    
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl Emu {
+    pub fn new() -> Self {
+        Self {
+            pc: START_ADDRESS,
+            ram: [0; RAM_SIZE],
+            screen: [false; SCREEN_HEIGHT * SCREEN_WIDTH],
+            v_reg: [0; NUM_REGISTERS],
+            i_reg: 0,
+            sp: 0,
+            stack: [0; STACK_SIZE],
+            keys: [false; NUM_KEYS],
+            dt: 0,
+            st: 0,
+        }
     }
 }
